a353db2ef83d29d49c867e84ab0b4474b42b2d55
ReKizzz <pyaephyomg2005zxc@gmail.com>
2025-04-14 00:40:33 +0630

    update color

--- src/modules/setting/entry/ColorUpdate.js
+++ src/modules/setting/entry/ColorUpdate.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import { colors, defaultColor, keys } from "../../../constants/config";
-import { Dropdown } from "primereact/dropdown";
-import { ChevronDownIcon } from "primereact/icons/chevrondown";
-import { ChevronRightIcon } from "primereact/icons/chevronright";
 import { setData } from "../../../helpers/localstorage";
 import { useDispatch, useSelector } from "react-redux";
 import { updateColor } from "../settingSlice";
@@ -11,71 +8,71 @@ export const ColorUpdate = () => {
   const [selectedColor, setColor] = useState(defaultColor);
 
   const dispatch = useDispatch();
-  const { translate } = useSelector(state => state.setting);
+  const { translate } = useSelector((state) => state.setting);
 
-  const onChangeColor = (e) => {
-    setColor(e);
-    setData(keys.COLOR, e);
-    dispatch(updateColor(e));
+  const onChangeColor = (colorObj) => {
+    setColor(colorObj);
+    setData(keys.COLOR, colorObj);
+    dispatch(updateColor(colorObj));
   };
 
-  const selectedColorTemplate = (option, props) => {
-    if (option) {
-      return (
-        <div className="flex align-items-center">
-          <div className="mr-2" style={{ width: "18px", height: "18px", backgroundColor: option.code, borderRadius: "50%" }}></div>
-          <div>{option.name}</div>
-        </div>
-      );
+  const handleColorInputChange = (e) => {
+    const pickedColor = e.target.value;
+    const matchedColor = colors.find(
+      (c) => c.code.toLowerCase() === pickedColor.toLowerCase()
+    );
+
+    if (matchedColor) {
+      onChangeColor(matchedColor);
+    } else {
+      onChangeColor({ name: "Custom", code: pickedColor });
     }
-    return <span>{props.placeholder}</span>;
-  };
-
-  const colorOptionTemplate = (option) => {
-    return (
-      <div className="flex align-items-center">
-        <div className="mr-2" style={{ width: "18px", height: "18px", backgroundColor: option.code, borderRadius: "50%" }}></div>
-        <div>{option.name}</div>
-      </div>
-    );
-  };
-
-  const panelFooterTemplate = () => {
-    return (
-      <div className="py-2 px-3">
-        {selectedColor ? (
-          <span>
-            <b>{selectedColor.name}</b> selected.
-          </span>
-        ) : (
-          "No color selected."
-        )}
-      </div>
-    );
   };
 
   return (
-    <div className="flex flex-column gap-2">
-      <label htmlFor="color" className="text-black">
+    <div className="flex flex-column gap-3">
+      <label htmlFor="colorPicker" className="text-black">
         {translate.change_color}
       </label>
-      <Dropdown
-        value={selectedColor}
-        onChange={(e) => onChangeColor(e.value)}
-        options={colors}
-        optionLabel="name"
-        placeholder="Select a Color"
-        valueTemplate={selectedColorTemplate}
-        itemTemplate={colorOptionTemplate}
-        panelFooterTemplate={panelFooterTemplate}
-        dropdownIcon={(opts) => {
-          return opts.iconProps["data-pr-overlay-visible"] ? (
-            <ChevronRightIcon {...opts.iconProps} />
-          ) : (
-            <ChevronDownIcon {...opts.iconProps} />
-          );
-        }}
-      />
+
+      {/* Color dropdown for predefined colors */}
+      <div className="flex flex-wrap gap-2">
+        {colors.map((color) => (
+          <button
+            key={color.code}
+            style={{
+              backgroundColor: color.code,
+              width: "30px",
+              height: "30px",
+              borderRadius: "50%",
+              border:
+                selectedColor.code === color.code
+                  ? "2px solid #000"
+                  : "1px solid #ccc",
+              cursor: "pointer",
+            }}
+            title={color.name}
+            onClick={() => onChangeColor(color)}
+          />
+        ))}
+      </div>
+
+      {/* Color input for custom colors */}
+      <div className="flex items-center gap-2 mt-2">
+        <input
+          type="color"
+          id="colorPicker"
+          value={selectedColor.code}
+          onChange={handleColorInputChange}
+          style={{
+            width: "40px",
+            height: "40px",
+            border: "none",
+            background: "none",
+          }}
+        />
+        <span className="text-sm">{selectedColor.name}</span>
+      </div>
     </div>
   );
 };
